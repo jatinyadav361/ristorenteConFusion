c64f403bc652cfa7f02addfe9611e5eebad9ec35
jatinyadav361 <jatinrao361@gmail.com>
2021-06-03 14:47:55 +0530

    Cookies

--- app.js
+++ app.js
@@ -32,29 +32,43 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req,res,next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if(!authHeader) {
-    var error = new Error('You are not authorized to access this.');
-    error.status = 401;
-    res.setHeader('WWW-Authenticate','Basic');
-    return next(error);
-  }
+  console.log(req.signedCookies);
 
-  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-  if(username === 'admin' && password === 'password') {
-    next();
+  if(!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if(!authHeader) {
+      var error = new Error('You are not authorized to access this.');
+      error.status = 401;
+      res.setHeader('WWW-Authenticate','Basic');
+      return next(error);
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+    if(username === 'admin' && password === 'password') {
+      res.cookie('user','admin',{signed : true});
+      next();
+    }
+    else {
+      var error = new Error('You are not authorized to access this');
+      error.status = 401;
+      res.setHeader('WWW-Authenticate','Basic');
+      return next(error);
+    }
   }
   else {
-    var error = new Error('You are not authorized to access this.');
-    error.status = 401;
-    res.setHeader('WWW-Authenticate','Basic');
-    return next(error);
+    if(req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var error = new Error('You are not authorized to access this');
+      error.status = 401;
+      return next(error);
+    }
   }
 }
 
