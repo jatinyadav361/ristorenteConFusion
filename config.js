a47e68f8f8a47677ecf1351b4413c0145c4b6b69
jatinyadav361 <jatinrao361@gmail.com>
2021-06-05 11:23:16 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '257684819441193',
+        clientSecret : 'b106259a3c76e1450d8e4e944501e6f6'
+    }
 }
