7cbcc1a6aca3c0cdafdc6e5e1ca8154f0452ec24
jatinyadav361 <jatinrao361@gmail.com>
2021-06-10 08:35:45 +0530

    3.2.1

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,56 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   console.log(`https://${req.hostname}/index.html`);
   res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+  return;
 });
 
-router.get('/:all', function(req, res, next) {
+router.get('/menu', function(req, res, next) {
   console.log(`https://${req.hostname}/index.html`);
   res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
 });
 
-router.get('/:all/:any', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   console.log(`https://${req.hostname}/index.html`);
   res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/cart', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/contactus', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/favorites', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/orders', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/aboutus', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
+});
+
+router.get('/menu/:id', function(req, res, next) {
+  console.log(`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
+  res.end('Redirected to homepage');
 });
 
 module.exports = router;
