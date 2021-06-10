202ef844134857c7970c8ed6ae9ff82b1e5977ee
jatinyadav361 <jatinrao361@gmail.com>
2021-06-10 09:06:15 +0530

    3.2.3

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,10 @@
 var express = require('express');
-const app = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/menu', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/home', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/cart', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/contactus', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/favorites', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/orders', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/aboutus', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
-});
-
-router.get('/menu/:id', function(req, res, next) {
-  console.log(`https://${req.hostname}/index.html`);
-  res.redirect(307,`https://${req.hostname}/index.html`);
+  res.redirect(`https://${req.hostname}/index.html`);
 });
 
 module.exports = router;
