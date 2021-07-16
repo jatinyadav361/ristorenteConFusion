215f6f939b27425919d6f74e02e9ca673e2e94bd
Jatin Kumar <jatinrao361@gmail.com>
2021-07-16 22:53:22 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : process.env.MONGODB_URI || 'mongodb+srv://jatinyadav361:BVMaXNA9878Ee766@confusion.d1g5l.mongodb.net/conFusionDatabase?retryWrites=true&w=majority',
-    // 'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook' : {
-        clientId : '257684819441193',
-        clientSecret : 'b106259a3c76e1450d8e4e944501e6f6'
-    }
-}
